aac46fcacde32ee8e4f0641ec16fb24674fa18b2
bitezhu <zhush1992@163.com>
2017-08-14 10:23:53 +0800

    add scatter

--- matplot.py
+++ matplot.py
@@ -10,10 +10,12 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.cluster.hierarchy import cophenet
 from scipy.spatial.distance import pdist
 from mpl_toolkits import axes_grid1
+import matplotlib.ticker as ticker
 
 from scipy import stats
 from itertools import *
 
+import seaborn as sns
 
 ## default style setting, some of them were replaced in matplotlib 2.0
 plt.rcParams['patch.force_edgecolor'] = False
@@ -35,9 +37,52 @@ plt.style.use(u'seaborn-bright')
 
 myfavcolors=['grey','tomato','peru','dodgerblue','brown','darkslategray','lightsalmon','limegreen','deeppink','mediumpurple','darkkhaki','aqua','coral','gold','cornflowerblue','crimson']
 mat2color=['#1f77b4','#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf']
-Markers = Line2D.markers
-Fillmarkers = Line2D.filled_markers
-myfavmarkers = [Markers['+'],Markers[2],Markers['x'],Markers[5],Markers['.'],Fillmarkers[0],Fillmarkers[2],Fillmarkers[-1],Fillmarkers[-6],Fillmarkers[-5],Markers['D']]
+
+'''
+============================== ===============================================
+marker                         description
+============================== ===============================================
+`"."`                          point
+`","`                          pixel
+`"o"`                          circle
+`"v"`                          triangle_down
+`"^"`                          triangle_up
+`"<"`                          triangle_left
+`">"`                          triangle_right
+`"1"`                          tri_down
+`"2"`                          tri_up
+`"3"`                          tri_left
+`"4"`                          tri_right
+`"8"`                          octagon
+`"s"`                          square
+`"p"`                          pentagon
+`"P"`                          plus (filled)
+`"*"`                          star
+`"h"`                          hexagon1
+`"H"`                          hexagon2
+`"+"`                          plus
+`"x"`                          x
+'''
+
+myfavmarkers = ['o','+','2','x','5','.','<','8','s']
+myfavcmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+'''
+==============================================================================
+==============================================================================
+# text style
+text_style = dict(horizontalalignment='right', verticalalignment='center',fontsize=12, fontdict={'family': 'monospace'})
+
+# text position
+from matplotlib.transforms import blended_transform_factory
+# the reference point (0 in Axes coords, y tick value in Data coords).
+reference_transform = blended_transform_factory(ax.transAxes, ax.transData)
+for i, (name, linestyle) in enumerate(linestyles.items()):
+    ax.annotate(str(linestyle), xy=(0.0, i), xycoords=reference_transform,
+                xytext=(-6, -12), textcoords='offset points', color="blue",
+                fontsize=8, ha="right", family="monospace")
+
+'''
 
 def styleNum(number):
     colors = cycle(myfavcolors)
@@ -63,6 +108,31 @@ def autolabel(rects,horizontal=0):
             height = rect.get_height()
             ax.text(rect.get_width()*1.02,rect.get_y()+height/2., '%d' % int(height),ha='center', va='center')
     return 0
+
+def setupTick(ax):
+    ax.spines['right'].set_color('none')
+    ax.spines['left'].set_color('none')
+    ax.yaxis.set_major_locator(ticker.NullLocator())   # remove all tickers,it seems that it shows only major ticks by default,so this line is enough normaly
+    ax.yaxis.set_minor_locator(ticker.NullLocator())
+    
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(0.5))  # show a major ticker every 0.5 space
+    ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+
+    majors = [0, 1, 5]
+    ax.xaxis.set_major_locator(ticker.FixedLocator(majors))  # also you can define any ticker position, even irregularly
+
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(4))  # MaxN Locator
+
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.tick_params(which='major', width=1.00)
+    ax.tick_params(which='major', length=5)
+    ax.tick_params(which='minor', width=0.75)
+    ax.tick_params(which='minor', length=2.5)
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 1)
+    ax.patch.set_alpha(0.0)
+
 
 def barplot(plotarr,arrlabel,xlabel="x",ylabel="y",title="barplot",picname="test",color='gray',log=0,horizontal=0,rotation=0,):
     fig = plt.figure()
@@ -100,7 +170,7 @@ def bargroup(matrix,samplenames,ticklabels,xlabel="x",ylabel="y",title="bargroup
     except AssertionError,e:
         sys.stderr.write("wrong data dimension!!!\n")
     if len(colors) != c:
-        colors,marker = styleNum(c)
+        colors,markers = styleNum(c)
     fig, ax = plt.subplots()
     ind = np.arange(r)
     for i in range(c):
@@ -124,9 +194,9 @@ def clearaxis(ax):
     ax.set_axis_off()
 
 def clearHalfaxis(ax):
+    '''Only show ticks on the left and bottom spines. '''
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-    # Only show ticks on the left and bottom spines
     ax.yaxis.set_ticks_position('left')
     ax.xaxis.set_ticks_position('bottom')
 
@@ -142,7 +212,7 @@ def boxplot(matrix,labels,xlabel="x",ylabel='y',title='boxplot',picname="test",r
     fig, ax = plt.subplots()
     #mat=np.asarray(matrix).T
     boxes=ax.boxplot(mat,notch=notch,patch_artist=patch_artist)
-    colors,marker = styleNum(len(labels))
+    colors,markers = styleNum(len(labels))
     for patch, color in zip(boxes['boxes'], colors):
         patch.set_facecolor(color)
     ax.set_xticks([i+1 for i in range(len(labels))],)
@@ -272,3 +342,98 @@ def clusterheatmap(mat,samplenames,featurenames,labelsize=14,plotxlabel=1,plotyl
     plt.clf()
     plt.close()
     return 0
+
+
+def corrheatplot(mat,samplenames,labelsize=10,picname='corrheat'):
+    r,c=mat.shape
+    try:
+        assert r==len(samplenames)
+    except AssertionError,e:
+        mat=mat.T
+        sys.stderr.write("inconsitent data dimension with length of labels!!! Will try to transpose the matrix\n")
+    corrMat = np.corrcoef(mat)
+    r,c = corrMat.shape
+    fig, ax = plt.subplots(figsize=(10,10))
+    my_norm = mpl.colors.Normalize(-1, 1)
+    cmap=sns.diverging_palette(220, 10, as_cmap=True)
+    im=ax.imshow(corrMat,aspect='auto', cmap=cmap,interpolation='nearest',norm=my_norm,)
+    ax.grid(False)
+    fontdict={'fontsize':labelsize}
+    rotationx=90
+    ax.set_xticks([i for i in range(r)],)
+    ax.set_xticklabels(samplenames,rotation=rotationx,fontdict=fontdict)
+    ax.set_yticks([i for i in range(r)],)
+    rotationx=0
+    ax.set_yticklabels(samplenames,rotation=rotationx,fontdict=fontdict)
+    ax.yaxis.set_ticks_position('right')
+    ax.tick_params(axis=u'both', which=u'both',length=0)
+
+    cax=fig.add_axes([0.04,0.70,0.01,0.25],frameon=False,)
+    plt.colorbar(im,cax=cax,)
+    cax.yaxis.set_ticks_position('right')
+    cax.yaxis.set_label_position('right')
+    plt.savefig("%s.png"%picname,format='png',dpi=300)
+    plt.savefig("%s.svg"%picname,format='svg',dpi=300)
+    plt.clf()
+    plt.close()
+    return 0
+
+
+def scatterplot(xarr,yarr,data,samplenames,colors=['C0','C1','C2'],marker=0,xlabel="x",ylabel='y',title='scatter plot',cm=0,picname="scatter",alpha=0.8,figsize=(8,6)):
+    c = len(xarr)
+    from sklearn import datasets
+    circles = datasets.make_circles()
+    try:
+        assert c == len(samplenames)
+    except AssertionError,e:
+        sys.stderr.write("inconsitent data dimension with length of samplenames!!!\n")
+    if len(colors) != c:
+        colors,markers = styleNum(c)
+    if not marker:
+        markers = ['o',]*c
+    fig, ax = plt.subplots(figsize=figsize)
+    xlimmin = min([min(i) for i in xarr])
+    xlimmax = max([max(i) for i in xarr])
+    ylimmin = min([min(i) for i in yarr])
+    ylimmax = max([max(i) for i in yarr])
+    cmap='RdYlBu_r'
+    #cmap=myfavcmap
+    vmax = np.ceil(max([max(i) for i in data]))
+    vmin = np.floor(min([min(i) for i in data]))
+                                                 #data = np.asarray(data,dtype=np.float64) in case of a non-matrix data
+    vmax = max([vmax,abs(vmin)]) # choose larger of vmin and vmax
+    my_norm = mpl.colors.Normalize(vmin, vmax)
+    for i in range(c):
+        if cm:
+            #ax.scatter(xarr[i], yarr[i],c=(0.2,0.4,0.6,0.8), s=data[i],marker=markers[i], alpha=0.8,label=samplenames[i], norm=my_norm)
+            im=ax.scatter(xarr[i], yarr[i],c=data[i], s=data[i],marker=markers[i], alpha=0.8,label=samplenames[i], cmap=cmap)
+            cax=fig.add_axes([0.04,0.70,0.01,0.25],frameon=False,)
+            plt.colorbar(im,cax=cax,)
+            '''
+            cax_yticks = np.linspace(vmin,vmax,6)
+            cax.set_yticks(cax_yticks)
+            cax.set_yticklabels(cax_yticks)
+            '''
+            tickL = cax.yaxis.get_ticklabels()
+            for t in tickL:
+                t.set_fontsize(t.get_fontsize() - 0.8)
+            cax.yaxis.set_ticks_position('right')
+            cax.yaxis.set_label_position('right')
+        else:
+            ax.scatter(xarr[i], yarr[i], s=data[i], c=colors[i], marker=markers[i], alpha=alpha, label=samplenames[i])
+            ax.legend(loc='best')
+    ax.set_title(title)
+    ax.set_xlim(xlimmin*0.85,xlimmax*1.1)
+    ax.set_ylim(ylimmin*0.85,ylimmax*1.1)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    #ax.legend(loc='best')
+    plt.savefig("%s.png"%picname,format='png',dpi=300)
+    plt.savefig("%s.svg"%picname,format='svg',dpi=300)
+    plt.clf()
+    plt.close()
+    return 0 
+
+
+def plot_chromosome():
+    pass
